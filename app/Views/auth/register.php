55aa2100761b1b94a789d89a7859f7f058bca729
MSAldi1170 <m.aldy1170@gmail.com>
2025-01-05 11:16:22 +0800

    he,,

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -31,7 +31,8 @@
         }
 
         .auth-content .text-center img {
-            margin-bottom: 5px; /* Jarak antara logo dan judul */
+            margin-bottom: 5px;
+            /* Jarak antara logo dan judul */
         }
 
         .card-body {
@@ -57,7 +58,6 @@
     </style>
 </head>
 
-<<<<<<< HEAD
 <div class="auth-wrapper">
     <div class="auth-content">
         <div class="card">
@@ -117,83 +117,16 @@
                                 <?php endif; ?>
                             </div>
                             <button class="btn btn-block btn-primary mb-4">Register</button>
-                            <p class="mb-0 text-muted">Sudah punya akun? <a href="/loginpendaftaran" class="f-w-400">Login</a></p>
+                            <p class="mb-0 text-muted">Sudah punya akun? <a href="/loginpendaftaran"
+                                    class="f-w-400">Login</a></p>
                         </form>
                     </div>
                 </div>
-=======
-<body>
-    <div class="auth-wrapper">
-        <div class="auth-content">
-            <div class="text-center mb-3">
-                <img src="assets/lp/img/logo.png" alt="SIPADES Logo" width="100">
-                <h4 class="mt-2">Register</h4> <!-- Gunakan margin kecil di sini -->
->>>>>>> f0d6dd9914d8164dc7d34534c0c986d19ec99cb0
             </div>
-            <h6 class="text-center text-muted mb-4">Sistem Informasi Pendaftaran dan Informasi Akademik</h6>
-            <?= session()->getFlashdata('pesan'); ?>
-            <form action="/register" method="post">
-                <div class="mb-3">
-                    <label for="nama" class="form-label">Email</label>
-                    <input type="text" class="form-control" name="nama" id="nama" value="<?= old('nama'); ?>" required>
-                    <?php if (session()->get('validation')): ?>
-                        <div class="text-danger">
-                            <?= session()->get('validation')->getError('nama') ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <!-- <div class="mb-3">
-                    <label for="no" class="form-label">Nama Lengkap</label>
-                    <input type="number" class="form-control" name="no_hp" id="no" value="<?= old('no_hp'); ?>" required>
-                    <?php if (session()->get('validation')): ?>
-                        <div class="text-danger">
-                            <?= session()->get('validation')->getError('no_hp') ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="mb-3">
-                    <label for="alamat" class="form-label">Alamat Lengkap</label>
-                    <input type="text" class="form-control" name="alamat" id="alamat" value="<?= old('alamat'); ?>" required>
-                    <?php if (session()->get('validation')): ?>
-                        <div class="text-danger">
-                            <?= session()->get('validation')->getError('alamat') ?>
-                        </div>
-                    <?php endif; ?>
-                </div> -->
-                <div class="mb-3">
-                    <label for="user" class="form-label">Username</label>
-                    <input type="text" class="form-control" name="username" id="user" value="<?= old('username'); ?>" required>
-                    <?php if (session()->get('validation')): ?>
-                        <div class="text-danger">
-                            <?= session()->get('validation')->getError('username') ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="mb-3">
-                    <label for="pass" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" id="pass" required>
-                    <?php if (session()->get('validation')): ?>
-                        <div class="text-danger">
-                            <?= session()->get('validation')->getError('password') ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="mb-3">
-                    <label for="konfpass" class="form-label">Ulangi Password</label>
-                    <input type="password" class="form-control" name="password1" id="konfpass" required>
-                    <?php if (session()->get('validation')): ?>
-                        <div class="text-danger">
-                            <?= session()->get('validation')->getError('password1') ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <button class="btn btn-block btn-primary mb-4">Register</button>
-                <p class="mb-0 text-muted">Sudah punya akun? <a href="/login" class="f-w-400">Login</a></p>
-            </form>
         </div>
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
